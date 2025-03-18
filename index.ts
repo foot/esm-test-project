6876ff715e27462c5b541c996f4b8cd6e642a1b0
Simon Howe <footless@gmail.com>
2025-03-18 16:51:57 +0100

    Add an ESM only module

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
 import * as k8s from "@pulumi/kubernetes";
+import * as k8sClient from "@kubernetes/client-node";
 
 const appLabels = { app: "nginx" };
 const deployment = new k8s.apps.v1.Deployment("nginx", {
-    spec: {
-        selector: { matchLabels: appLabels },
-        replicas: 1,
-        template: {
-            metadata: { labels: appLabels },
-            spec: { containers: [{ name: "nginx", image: "nginx" }] }
-        }
-    }
+  spec: {
+    selector: { matchLabels: appLabels },
+    replicas: 1,
+    template: {
+      metadata: { labels: appLabels },
+      spec: { containers: [{ name: "nginx", image: "nginx" }] },
+    },
+  },
 });
+
+const kc = new k8sClient.KubeConfig();
+kc.loadFromDefault();
+
 export const name = deployment.metadata.name;
